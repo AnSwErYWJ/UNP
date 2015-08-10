88e08070ef98aedaed6b2a24861dcc0dd25f1069
AnSwErYWJ <ywj1993@yeah.net>
2015-08-10 17:27:06 +0800

    TCP回射程序

--- TCP/server.c
+++ TCP/server.c
@@ -67,8 +67,8 @@ void sig_child(int signo)
     pid_t pid;
     int stat;
 
-    pid = wait(&stat);
-    printf("child %d terminated\n",pid);
+    while((pid = waitpid(-1,&stat,WNOHANG)))//等待任一子进程
+        printf("child %d terminated\n",pid);
 }
 
 int main(void)
